d34eabf4119fb7f207e48119f16a2d77df6b3d26
Jose <josejesuscedeno@gmail.com>
2022-02-10 16:45:51 +0000

    comienza a generarse los numeros

--- src/app.js
+++ src/app.js
@@ -2,10 +2,28 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#numbercard").innerHTML = Numbergenerator();
+  });
+  console.log("Hello Rigo from the console! ");
+};
+let Numbergenerator = () => {
+  let Number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numberIndex = Math.floor(Math.random() * Number.length);
+  return Number[numberIndex];
 };
