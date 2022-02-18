1e3d0637826aa337fa556b0526fbc8016f72442e
Jose <josejesuscedeno@gmail.com>
2022-02-18 17:01:56 +0000

    carta funcionando, falta que se visualice al cargar la pagina

--- src/app.js
+++ src/app.js
@@ -1,15 +1,7 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-//window.onload = () => {
-//document.querySelector("#btn").addEventListener("click", () => {
-//document.querySelector("#numbercard").innerHTML = Numbergenerator();
-//});
-//};
-//let Numbergenerator = () => {let Number = ["A","2","3","4","5","6","7","8","9","10","J", "Q","K"];
-//let numberIndex = Math.floor(Math.random() * Number.length);
-//return Number[numberIndex];};
+//I generate the Event
 window.onload = () => {
   document.querySelector("#btn").addEventListener("click", () => {
     document.querySelector(".card").innerHTML = Suitgenerator();
@@ -17,7 +9,7 @@ window.onload = () => {
   console.log("I am generating the suit");
 };
 let Suitgenerator = () => {
-  let SuitArray = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  let SuitArray = ["'>&spades;", "'>&clubs;", "red'>&hearts;", "red'>&diams;"];
   let suitIndex = Math.floor(Math.random() * SuitArray.length);
   let NumberArray = [
     "A",
@@ -35,14 +27,14 @@ let Suitgenerator = () => {
     "K"
   ];
   let numberIndex = Math.floor(Math.random() * NumberArray.length);
-  let red = document.querySelector(".suitcard");
+
   //red.style.color = "red";
   //a partir de aqui hago el proceso usando lets
   let suitSymbol = SuitArray[suitIndex];
   let numberSymbol = NumberArray[numberIndex];
-  let suitup = "<span class='topsuit'>" + suitSymbol + "</span>";
+  let suitup = "<span class='topsuit " + suitSymbol + "</span>";
   let number = "<h1 id='numbercard' class='number'>" + numberSymbol + "</h1>";
-  let suitdown = "<span class='bottomsuit'>" + suitSymbol + "</span>";
+  let suitdown = "<span class='bottomsuit " + suitSymbol + "</span>";
 
   let actualCard = suitup + number + suitdown;
   return actualCard;
